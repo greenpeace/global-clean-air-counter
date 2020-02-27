f12a79104eb8681ed2a74ba84c0eb282b52b66b9
strophy <leon.white@dash.org>
2020-02-27 16:24:34 +1300

    Delete code related to ozone calculations

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -1,32 +1,6 @@
 <template>
     <div v-if="this.$parent.cityData">
-        <i18n path="pm25no2o3deaths" tag="h2" id="costtext" class="mb-2" v-if="no2 && o3">
-            <template #city>
-                <strong class="bg-warning">{{ $t('cities.' + cityData.cityID) }}</strong>
-            </template>
-            <template #costLine><br />
-                <span class="bg-warning font-weight-bold">{{ totalDeaths(cityData).toLocaleString() }} {{ $t('life_cost')}}</span>
-                <span class="bg-transparent"> {{ $t('and') }} </span>
-                <span class="bg-warning font-weight-bold">US${{ totalCosts(cityData).toLocaleString() }}</span><br />
-            </template>
-            <template #showDate>
-                <span>{{ $d(new Date(2020, 0, 1), 'long') }}</span>
-            </template>
-        </i18n>
-        <i18n path="pm25no2deaths" tag="h2" id="costtext" class="mb-2" v-else-if="no2">
-            <template #city>
-                <strong class="bg-warning">{{ $t('cities.' + cityData.cityID) }}</strong>
-            </template>
-            <template #costLine><br />
-                <span class="bg-warning font-weight-bold">{{ totalDeaths(cityData).toLocaleString() }} {{ $t('life_cost')}}</span>
-                <span class="bg-transparent"> {{ $t('and') }} </span>
-                <span class="bg-warning font-weight-bold">US${{ totalCosts(cityData).toLocaleString() }}</span><br />
-            </template>
-            <template #showDate>
-                <span>{{ $d(new Date(2020, 0, 1), 'long') }}</span>
-            </template>
-        </i18n>
-        <i18n path="pm25o3deaths" tag="h2" id="costtext" class="mb-2" v-else-if="o3">
+        <i18n path="pm25no2deaths" tag="h2" id="costtext" class="mb-2" v-if="no2">
             <template #city>
                 <strong class="bg-warning">{{ $t('cities.' + cityData.cityID) }}</strong>
             </template>
@@ -72,8 +46,7 @@ export default {
     props: ['cityData'],
     data: function() {
         return {
-            no2: false,
-            o3: false
+            no2: false
         }
     },
     methods: {
@@ -81,17 +54,15 @@ export default {
             // Get all data resulting in deaths
             var pm25Deaths = (cityData.estimations.ytd['PM2.5'].filter(x => x.Outcome === 'Deaths')).filter(y => y.Cause.indexOf('LRI') >= 0)
             var no2Deaths = cityData.estimations.ytd['NO2'].filter(x => x.Outcome === 'Deaths')
-            var o3Deaths = cityData.estimations.ytd['O3_8h'].filter(x => x.Outcome === 'Deaths')
             var totalDeaths = 0
 
             no2Deaths[0].number_central ? this.no2 = true : this.no2 = false
-            o3Deaths[0].number_central ? this.o3 = true : this.o3 = false
 
             // Add up deaths and check for existence of other causes of death
             for (var death in pm25Deaths) {
                 totalDeaths += pm25Deaths[death].number_central
             }
-            totalDeaths += (this.no2 ? no2Deaths[0].number_central : 0) + (this.o3 ? o3Deaths[0].number_central : 0)
+            totalDeaths += (this.no2 ? no2Deaths[0].number_central : 0)
             
             return Math.round(totalDeaths)
         },
@@ -102,18 +73,16 @@ export default {
             pm25costs.push(cityData.estimations.ytd['PM2.5'].filter(x => x.Outcome === 'YLDs')[0])
             pm25costs.push(cityData.estimations.ytd['PM2.5'].filter(x => x.Outcome === 'PTB')[0])
             var no2Costs = cityData.estimations.ytd['NO2'].filter(x => x.Outcome === 'YLLs')
-            var o3Costs = cityData.estimations.ytd['O3_8h'].filter(x => x.Outcome === 'YLLs')
             var totalCosts = 0
             
             // Update flags if cost data overrides deaths, other keep same
             no2Costs[0]['cost.USD_central'] ? this.no2 = true : this.no2 = this.no2
-            o3Costs[0]['cost.USD_central'] ? this.o3 = true : this.o3 = this.o3
 
             // Add up costs and check for existence of other costs
             for (var cost in pm25costs) {
                 totalCosts += pm25costs[cost]['cost.USD_central']
             }
-            totalCosts += (no2Costs[0]['cost.USD_central'] ? no2Costs[0]['cost.USD_central'] : 0) + (o3Costs[0]['cost.USD_central'] ? o3Costs[0]['cost.USD_central'] : 0)
+            totalCosts += (no2Costs[0]['cost.USD_central'] ? no2Costs[0]['cost.USD_central'] : 0)
 
             return Math.round(totalCosts)
         }
