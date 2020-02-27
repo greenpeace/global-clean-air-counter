9f1c4795c53f1b5b301b56ba194ca34634a07156
strophy <leon.white@dash.org>
2020-02-27 14:50:35 +1300

    Clean up debug

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -67,7 +67,6 @@
 </template>
 
 <script>
-/* eslint-disable */
 export default {
     name: 'ShowData',
     props: ['cityData'],
@@ -97,6 +96,7 @@ export default {
             return Math.round(totalDeaths)
         },
         totalCosts: function(cityData) {
+            // Get all data resulting in costs
             var pm25costs = (cityData.estimations.ytd['PM2.5'].filter(x => x.Outcome === 'YLLs')).filter(y => y.Cause.indexOf('LRI') >= 0)
             pm25costs.push(cityData.estimations.ytd['PM2.5'].filter(x => x.Outcome === 'Absences')[0])
             pm25costs.push(cityData.estimations.ytd['PM2.5'].filter(x => x.Outcome === 'YLDs')[0])
@@ -104,17 +104,12 @@ export default {
             var no2Costs = cityData.estimations.ytd['NO2'].filter(x => x.Outcome === 'YLLs')
             var o3Costs = cityData.estimations.ytd['O3_8h'].filter(x => x.Outcome === 'YLLs')
             var totalCosts = 0
-
-            console.log('pm25costs: ')
-            console.log(pm25costs)
-            console.log('no2Costs: ')
-            console.log(no2Costs)
-            console.log('o3Costs: ')
-            console.log(o3Costs)
             
-            //no2Costs[0].cost.USD_central ? this.no2 = true : this.no2 = false
-            //o3Costs[0].cost.USD_central ? this.o3 = true : this.o3 = false
+            // Update flags if cost data overrides deaths, other keep same
+            no2Costs[0].cost.USD_central ? this.no2 = true : this.no2 = this.no2
+            o3Costs[0].cost.USD_central ? this.o3 = true : this.o3 = this.o3
 
+            // Add up costs and check for existence of other costs
             for (var cost in pm25costs) {
                 totalCosts += pm25costs[cost]['cost.USD_central']
             }
