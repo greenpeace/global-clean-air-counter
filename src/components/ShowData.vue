0de8e0facb21b99f7086f7a378fa63d1c4ecfa95
strophy <leon.white@dash.org>
2020-03-13 16:44:59 +1300

    Only process data if it exists

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -87,18 +87,20 @@ export default {
         }
     },
     updated: function () {
-        var shareText = document.getElementById('costtext').textContent;
-        var shareHashtag = ''
-        if (this.$t('hashtag') === 'CleanAirNow') {
-            shareHashtag = 'CleanAirNow'
+        if (this.$parent.cityData) {
+            var shareText = document.getElementById('costtext').textContent;
+            var shareHashtag = ''
+            if (this.$t('hashtag') === 'CleanAirNow') {
+                shareHashtag = 'CleanAirNow'
+            }
+            else {
+                shareHashtag = this.$t('hashtag') + ',CleanAirNow'
+            }
+            var twitterShare = 'https://twitter.com/intent/tweet?text=' + shareText + '&url=' + window.location.href + '&hashtags=' + shareHashtag;
+            var facebookShare = 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.href;
+            document.getElementById('twittersharebutton').href = twitterShare
+            document.getElementById('facebooksharebutton').href = facebookShare
         }
-        else {
-            shareHashtag = this.$t('hashtag') + ',CleanAirNow'
-        }
-        var twitterShare = 'https://twitter.com/intent/tweet?text=' + shareText + '&url=' + window.location.href + '&hashtags=' + shareHashtag;
-        var facebookShare = 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.href;
-        document.getElementById('twittersharebutton').href = twitterShare
-        document.getElementById('facebooksharebutton').href = facebookShare
     }
 }
 </script>
