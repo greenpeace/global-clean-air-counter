0cf135622b6941f5084db6863e7ac61f7c645ac9
strophy <leon.white@dash.org>
2020-03-13 16:27:56 +1300

    Add loading indicator

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     <div id="cityinfo" class="jumbotron border bg-transparent" style="border-width: 5px !important; padding: 1rem !important; text-align: center;">
         <h2 class="mb-3">{{ $t('lead_text') }}</h2>
         <Dropdown @update:option="getData" :cityList=this.cityList class="mb-4"></Dropdown>
+        <RingLoader :color="'#66cc00'" :loading=loadingState />
         <ShowData :cityData=cityData></ShowData>
         <i18n path="created" tag="p">
             <template #greenpeace>
@@ -17,25 +18,30 @@
 <script>
 import Dropdown from './components/Dropdown.vue'
 import ShowData from './components/ShowData.vue'
+import RingLoader from '@bit/joshk.vue-spinners-css.ring-loader'
 export default {
     name: 'counter',
     components: {
         Dropdown,
         ShowData,
+        RingLoader
     },
     methods: {
         getData: function(value) {
             //const url = 'https://y2mta1n6vj.execute-api.us-east-1.amazonaws.com/prod/pollutionCostCounter/' + value
             const url = 'https://website-api.airvisual.com/v1/analytics/counter/' + value
+            this.loadingState = true
             this.axios
                 .get(url)
-                .then(response => (this.cityData = response.data));
+                .then(response => (this.cityData = response.data))
+                .then(this.loadingState = false);
         }
     },
     data: function() {
         return {
             cityData: null,
-            cityList: this.$attrs.citylist
+            cityList: this.$attrs.citylist,
+            loadingState: false
         }
     },
     mounted: function () {
