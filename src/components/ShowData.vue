82754f071e9f80cc97b4af0198534697fb4fafe0
Candice Chan <cachan@greenpeace.org>
2022-04-13 23:56:08 +0800

    ShowData -> testing

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -46,6 +46,7 @@ export default {
     },
     methods: {
         totalDeaths: function(cityData) {
+            console.log(cityData)
             // Get all data resulting in deaths
             var pm25Deaths = (cityData.estimations.ytd['PM2.5'].filter(x => x.Outcome === 'Deaths')).filter(y => y.Cause.indexOf('LRI') >= 0)
             // Exclude NO2 data
